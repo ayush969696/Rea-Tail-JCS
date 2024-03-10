66dd58ad4d6a7a5d87bd82e209336c2f6438eef1
ayush969696 <ayush972982@gamil.com>
2024-03-10 21:20:13 +0530

    createing new Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-500'>Hello! It's Tailwind React Js</h1>
+    </>
+  )
+}
+
+export default App
